9ce618ad864df608f3f8ae98b8224c739457773c
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2020-01-28 19:11:34 -0800

    wrote pad class method. refactored clickHandler

---
+++ react_odometer/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Odometer from './odometer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Odometer />
+    </div>
+  );
+}
+
+export default App;
---
+++ react_odometer/src/odometer.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+class Odometer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            miles: 0,
+        }
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    clickHandler(inc) {
+        console.log(inc)
+        this.setState({
+            miles: this.state.miles + inc
+        });
+    }
+
+    pad(num, digits){
+        
+        let stringified = num.toString()
+      
+        let numZeros = digits - stringified.length
+        let str = ''
+        for(let i=0; i<numZeros; i++){
+          str += '0'
+        }
+        str += stringified
+        return str
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Odometer Goes Here</h1>
+                <p>{this.pad(this.state.miles, 4)}</p>
+                <div>Ones: 
+                    <button onClick={this.clickHandler.bind(this, 1)}> + </button>
+                    <button onClick={this.clickHandler.bind(this, -1)}> - </button>
+                </div>
+                <div>Tens: 
+                    <button onClick={this.clickHandler.bind(this, 10)}> + </button>
+                    <button onClick={this.clickHandler.bind(this, -10)}> - </button>
+                </div>
+                <div>Hundreds: 
+                    <button onClick={this.clickHandler.bind(this, 100)}> + </button>
+                    <button onClick={this.clickHandler.bind(this, -100)}> - </button>
+                </div>
+                <div>Thousands: 
+                    <button onClick={this.clickHandler.bind(this, 1000)}> + </button>
+                    <button onClick={this.clickHandler.bind(this, -1000)}> - </button>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Odometer
